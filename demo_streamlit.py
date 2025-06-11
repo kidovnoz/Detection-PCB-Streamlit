5e8f0be2fd351a68cba467fdf8629c97d116f292
toan.diepthe <toan.thediep@meiko.vn>
2025-06-11 16:24:12 +0700

    update

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from ultralytics import YOLO
+from ultralytics import YOLO, SAM
 from PIL import Image
 import numpy as np
 import yaml
@@ -8,9 +8,11 @@ import asyncio
 import torch
 import cv2
 import torchvision
+import time
 
 # Cài đặt tương thích với Windows và Python >= 3.8
-if sys.platform.startswith('win') and sys.version_info >= (3, 8):
+start_time = time.time()
+if sys.platform.startswith("win") and sys.version_info >= (3, 8):
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 st.title("AOI-AI Meiko Automation")
@@ -23,16 +25,19 @@ if torch.cuda.is_available():
 else:
     st.sidebar.warning("⚠️ Không tìm thấy GPU, đang chạy trên CPU.")
 
+
 # Load file YAML cấu hình model
 def load_model_config(config_path):
     with open(config_path, "r", encoding="utf-8") as f:
         config = yaml.safe_load(f)
     model_paths = [model["weight_path"] for model in config["models"]]
     model_names = [model["class_name"] for model in config["models"]]
-    return model_paths, model_names
+    model_segment = [model["weight_path"] for model in config["model_segment"]]
+    return model_paths, model_names, model_segment
 
-yaml_path = "config.yaml"
-st.logo("meiko-logo.webp", size="large", link="https://meiko-elec.com.vn/")
+
+yaml_path = r"D:\AOI-MKAC\MixPL\src\aoi_inspection\configs\config_streamlit.yaml"
+st.logo("D:/meiko-logo.webp", size="large", link="https://meiko-elec.com.vn/")
 
 # Sidebar settings
 st.sidebar.header("⚙️ Cấu hình")
@@ -50,22 +55,29 @@ password_input = st.sidebar.text_input("🔑 Nhập mật khẩu", type="passwor
 if password_input == ADMIN_PASSWORD:
     st.sidebar.success("✅ Đăng nhập thành công!")
     yaml_path = st.sidebar.text_input(" 🔧 Đường dẫn file YAML", value=yaml_path)
-    model_paths, model_names = load_model_config(yaml_path)
-    
+    model_paths, model_names, model_segment = load_model_config(yaml_path)
+
 else:
     st.sidebar.warning("🔐 Nhập mật khẩu để xem cấu hình")
-    model_paths, model_names = load_model_config(yaml_path)
+    model_paths, model_names, model_segment = load_model_config(yaml_path)
+
 
 # ✅ Load model chỉ 1 lần
 @st.cache_resource
 def load_models(paths):
     return [YOLO(path) for path in paths]
 
-models = load_models(model_paths)
 
+def load_model_segment(paths):
+    return [SAM(path) for path in paths]
+
+
+models = load_models(model_paths)
+models_segment = load_model_segment(model_segment)
 # Lưu kết quả đã xử lý vào session_state
 if "processed_images" not in st.session_state:
     st.session_state.processed_images = {}
+
 
 # Xử lý ảnh đầu vào
 def process_image(image_file, models, model_names, confidence):
@@ -79,11 +91,7 @@ def process_image(image_file, models, model_names, confidence):
             try:
                 model.eval()
                 results = model.predict(
-                    source=image,
-                    conf=confidence,
-                    imgsz=640,
-                    device='cpu',
-                    augment=True
+                    source=image, conf=confidence, imgsz=640, device=0, augment=True
                 )
 
                 for r in results:
@@ -92,14 +100,37 @@ def process_image(image_file, models, model_names, confidence):
                             x1, y1, x2, y2 = map(float, box.xyxy[0].tolist())
                             conf_score = float(box.conf.item())
                             cls = int(box.cls.item())
-                            
-                            all_boxes.append({
-                                "box": [x1, y1, x2, y2],
-                                "conf": conf_score,
-                                "model_idx": i,
-                                "labels": [f"{cls}: {conf_score:.2f}"]
-                            })
+                            if conf_score > 0.7:
+                                # Dùng SAM để segment bbox
+                                sam_model = models_segment[0]
+                                segment_result = sam_model(
+                                    source=image_array, bboxes=[[x1, y1, x2, y2]]
+                                )
 
+                                # Lấy mask đầu tiên
+                                mask = segment_result[0].masks.data[0].cpu().numpy()
+                                mask = mask.astype(np.uint8)
+                                mask = cv2.resize(
+                                    mask,
+                                    (image_array.shape[1], image_array.shape[0]),
+                                    interpolation=cv2.INTER_NEAREST,
+                                )
+
+                                pixel_count = len(mask)  # Đếm số pixel
+                                print(
+                                    f"Bounding box [{x1:.0f}, {y1:.0f}, {x2:.0f}, {y2:.0f}] -> {pixel_count} pixels"
+                                )
+
+                                all_boxes.append(
+                                    {
+                                        "box": [x1, y1, x2, y2],
+                                        "conf": conf_score,
+                                        "model_idx": i,
+                                        "labels": [f"{cls}: {conf_score:.2f}"],
+                                        "pixel_count": pixel_count,
+                                        "mask": mask,
+                                    }
+                                )
 
             except Exception as e:
                 st.error(f"Lỗi predict model {i}: {e}")
@@ -110,7 +141,7 @@ def process_image(image_file, models, model_names, confidence):
                 "image_id": image_file.name,
                 "image_show": image,
                 "found": False,
-                "label": "Không phát hiện"
+                "label": "Không phát hiện",
             }
 
         # Tạo tensor để chạy NMS
@@ -128,10 +159,52 @@ def process_image(image_file, models, model_names, confidence):
         draw_img = image_array.copy()
         for b in final_boxes:
             x1, y1, x2, y2 = map(int, b["box"])
-            label = f"{model_names[b['model_idx']]}: {b['conf']:.2f}"
-            cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(draw_img, label, (x1, y1 - 10),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            label = f"{model_names[b['model_idx']]}: {b['conf']:.2f} ({b['pixel_count']} px)"
+
+            # Vẽ bbox
+            # cv2.rectangle(draw_img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.putText(
+                draw_img,
+                label,
+                (x1, y1 - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (255, 0, 0),
+                2,
+            )
+            x_min = b["box_mask_x_min"] = np.min(np.where(b["mask"] == 1)[1])
+            x_max = b["box_mask_x_max"] = np.max(np.where(b["mask"] == 1)[1])
+            y_min = b["box_mask_y_min"] = np.min(np.where(b["mask"] == 1)[0])
+            y_max = b["box_mask_y_max"] = np.max(np.where(b["mask"] == 1)[0])
+
+            width = x_max - x_min
+            height = y_max - y_min
+
+            center_x = int((x_min + x_max) / 2)
+            center_y = int((y_min + y_max) / 2)
+
+            cv2.rectangle(draw_img, (x_min, y_min), (x_max, y_max), (0, 0, 255), 2)
+
+            # Vẽ đường ngang (trục x) qua tâm
+            cv2.line(draw_img, (x_min, center_y), (x_max, center_y), (0, 255, 255), 1)
+
+            # Vẽ đường dọc (trục y) qua tâm
+            cv2.line(draw_img, (center_x, y_min), (center_x, y_max), (255, 0, 255), 1)
+
+            # Overlay mask
+            mask = b.get("mask", None)
+            if mask is not None:
+                # Resize mask về đúng shape ảnh nếu cần
+                mask_resized = cv2.resize(
+                    mask.astype(np.uint8), (draw_img.shape[1], draw_img.shape[0])
+                )
+
+                # Tạo màu overlay (ví dụ màu đỏ)
+                colored_mask = np.zeros_like(draw_img)
+                colored_mask[:, :, 2] = (mask_resized * 255).astype(np.uint8)
+
+                # Tô nhẹ mask lên ảnh
+                draw_img = cv2.addWeighted(draw_img, 1.0, colored_mask, 0.4, 0)
 
         final_image = Image.fromarray(draw_img)
 
@@ -139,7 +212,8 @@ def process_image(image_file, models, model_names, confidence):
             "image_id": image_file.name,
             "image_show": final_image,
             "found": True,
-            "label": f"{len(final_boxes)} vùng lỗi"
+            "label": f"{len(final_boxes)} vùng lỗi",
+            "pixel": [f"Pixel: {pixel_count}"],
         }
 
     except Exception as e:
@@ -148,11 +222,16 @@ def process_image(image_file, models, model_names, confidence):
             "image_id": image_file.name,
             "image_show": None,
             "found": False,
-            "label": "Lỗi xử lý"
+            "label": "Lỗi xử lý",
         }
 
+
 # Upload nhiều ảnh
-uploaded_files = st.file_uploader("📁 Chọn ảnh đầu vào", type=["jpg", "jpeg", "png", "bmp"], accept_multiple_files=True)
+uploaded_files = st.file_uploader(
+    "📁 Chọn ảnh đầu vào",
+    type=["jpg", "jpeg", "png", "bmp"],
+    accept_multiple_files=True,
+)
 num_cols = 1
 
 if uploaded_files:
@@ -171,15 +250,17 @@ if uploaded_files:
 
         col = cols[idx % num_cols]
         with col:
-                st.markdown(f"**Ảnh {idx+1}**")
+            st.markdown(f"**Ảnh {idx+1}**")
 
-                # Hiển thị ảnh
-                if result["image_show"] is not None:
-                    st.image(result["image_show"], use_container_width=True, output_format="auto")
+            # Hiển thị ảnh
+            if result["image_show"] is not None:
+                st.image(
+                    result["image_show"], use_container_width=True, output_format="auto"
+                )
 
-                # Hiển thị label với chiều cao cố định, ellipsis nếu quá dài
-                st.markdown(
-                    f"""
+            # Hiển thị label với chiều cao cố định, ellipsis nếu quá dài
+            st.markdown(
+                f"""
                     <div style='height: 15px; 
                                 display: flex; 
                                 align-items: center; 
@@ -192,17 +273,15 @@ if uploaded_files:
                                 white-space: nowrap;'>
                         {result['label']}
                     </div>
-                    """, 
-                    unsafe_allow_html=True
-                )
+                    """,
+                unsafe_allow_html=True,
+            )
 
-                # Hiển thị icon ✅/❌ ở dòng riêng, luôn nằm dưới
-                st.markdown(
-                    f"<div style='text-align: center; font-size: 24px;'>{'✅' if result['found'] else '❌'}</div>", 
-                    unsafe_allow_html=True
-                )
-
-
+            # Hiển thị icon ✅/❌ ở dòng riêng, luôn nằm dưới
+            st.markdown(
+                f"<div style='text-align: center; font-size: 24px;'>{'✅' if result['found'] else '❌'}</div>",
+                unsafe_allow_html=True,
+            )
 
         if (idx + 1) % num_cols == 0 and idx + 1 != len(uploaded_files):
             cols = st.columns(num_cols)
@@ -213,3 +292,4 @@ if uploaded_files:
         for _ in range(num_cols - remainder):
             with st.columns(1)[0]:
                 st.empty()
+    print(f"Process completed in {time.time() - start_time:.2f} seconds.")
